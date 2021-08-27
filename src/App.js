2bfaea25b5d928b45fd7927988be7415c6fb9490
wby <sssssinter@qq.com>
2021-08-28 00:06:06 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import { useCallback, useRef } from "react";
+import { Rnd } from "react-rnd";
+import "./App.css";
+import Grid from "./Grid";
+import useWindowSize from "./resizeWindow";
+
+function getNodePoints(node) {
+  const { x, y, width, height } = node.getBoundingClientRect();
+  return {
+    topLeft: { x, y },
+    topRight: { x: x + width, y },
+    bottomLeft: { x, y: y + height },
+    bottomRight: { x: x + width, y: y + height },
+  };
+}
+
+function findNearestAnchor(pt, spanCount, gutter, windowSize) {
+  const spanWidth = (windowSize.width + gutter) / spanCount - gutter;
+  const wLoc = Math.round(pt.x / (spanWidth + gutter));
+  const hLoc = Math.round(pt.y / (spanWidth + gutter));
+  return { x: wLoc * (spanWidth + gutter), y: hLoc * (spanWidth + gutter) };
+}
 
 function App() {
+  const spanCount = 24;
+  const gutter = 12;
+  const windowSize = useWindowSize();
+  const rndRef = useRef();
+  const handleDragStop = useCallback(
+    (e, data) => {
+      const { topLeft } = getNodePoints(data.node);
+      rndRef.current.updatePosition(
+        findNearestAnchor(topLeft, spanCount, gutter, windowSize)
+      );
+    },
+    [spanCount, gutter, windowSize]
+  );
+
+  const handleResizeStop = useCallback(
+    (e, dir, refToElement, delta, position) => {
+      console.log(dir, delta, position);
+    },
+    []
+  );
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div style={{ width: "100vw", height: "100vh", position: "relative" }}>
+      <Grid spanCount={spanCount} gutter={gutter} windowSize={windowSize} />
+      <Rnd
+        ref={rndRef}
+        default={{
+          x: 0,
+          y: 0,
+          width: 320,
+          height: 200,
+        }}
+        style={{
+          backgroundColor: "rgba(122,112,200,0.3)",
+        }}
+        bounds="parent"
+        onDragStop={handleDragStop}
+        onResizeStop={handleResizeStop}
+      >
+        <div
+          style={{
+            justifyContent: "center",
+            alignItems: "center",
+            height: "100%",
+            width: "100%",
+            display: "flex",
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          <div>Rnd</div>
+        </div>
+      </Rnd>
     </div>
   );
 }
---
+++ src/Grid.js
@@ -0,0 +1,84 @@
+import React, { useMemo } from "react";
+
+function Grid({ gutter, spanCount, windowSize }) {
+  return useMemo(() => {
+    const toRenderHor = [];
+    for (let i = 0; i < spanCount - 1; ++i) {
+      toRenderHor.push(1);
+      toRenderHor.push(2);
+    }
+    toRenderHor.push(1);
+    const spanHeight = (windowSize.width + gutter) / spanCount - gutter;
+    const verticalSpanCount = Math.ceil(
+      (windowSize.height + gutter) / (spanHeight + gutter)
+    );
+    const toRenderVert = [];
+    for (let i = 0; i < verticalSpanCount - 1; ++i) {
+      toRenderVert.push(1);
+      toRenderVert.push(2);
+    }
+    toRenderVert.push(1);
+    return (
+      <div
+        style={{ position: "absolute", top: 0, left: 0, right: 0, bottom: 0 }}
+      >
+        <div
+          style={{ position: "absolute", top: 0, left: 0, right: 0, bottom: 0 }}
+        >
+          <div style={{ height: "100%", width: "100%", display: "flex" }}>
+            {toRenderHor.map((each, i) => {
+              if (each === 1) {
+                return (
+                  <div
+                    key={i}
+                    style={{
+                      flex: "1",
+                      height: "100%",
+                    }}
+                  ></div>
+                );
+              }
+              return (
+                <div
+                  key={i}
+                  style={{
+                    width: `${gutter}px`,
+                    height: "100%",
+                    background: "lightgrey",
+                    opacity: 0.3,
+                  }}
+                ></div>
+              );
+            })}
+          </div>
+        </div>
+        <div style={{ height: "100%", width: "100%", overflow: "hidden" }}>
+          {toRenderVert.map((each, i) => {
+            if (each === 1) {
+              return (
+                <div
+                  key={i}
+                  style={{
+                    height: `${spanHeight}px`,
+                  }}
+                ></div>
+              );
+            }
+            return (
+              <div
+                key={i}
+                style={{
+                  height: `${gutter}px`,
+                  background: "lightgrey",
+                  opacity: 0.3,
+                }}
+              ></div>
+            );
+          })}
+        </div>
+      </div>
+    );
+  }, [gutter, spanCount, windowSize]);
+}
+
+export default Grid;
